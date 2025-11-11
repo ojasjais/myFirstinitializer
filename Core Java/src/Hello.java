6c1221a9e07a84944045630ea2e67badb99c34bb
Ojasvi Jaiswal <pankhudij19@gmail.com>
2025-11-11 18:12:42 +0530

    Initial commit: Added Hello.java

---
+++ Core Java/src/Hello.java
@@ -0,0 +1,70 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Printing Primitive Datatype");
+
+       int num1 = 3;
+       System.out.println("Integer: "+ num1);
+
+        float num2  = 1.2f;
+        String name ="ojasvi";
+        char c='J';
+        double d2=5.7;
+        boolean b=true;
+
+        System.out.println("Float: "+num2);
+        System.out.println("Character: "+c);
+        System.out.println("Double: "+d2);
+        System.out.println("String: "+name);
+
+        //TypeCasting
+       int n= 10;
+       double d=n;
+        System.out.println("Widening: "+d);
+
+        double d1=10.99;
+        int num=(int)d1;
+        System.out.println("Narrowing "+num);
+
+        float f1=10.00f;
+        float f2=10.00f;
+        int result=(int)f1*(int)f2;
+        System.out.println("Result: "+result);
+
+
+        //operations : Arithmetic ,relational , logical
+        int x=10,y=5;
+        if(x>=y){
+            System.out.println("Print x: "+ x);
+        }
+        else{
+            System.out.println("Print y: " + y);
+        }
+
+        if(x<10 && y>=20 )
+        {
+            System.out.println("hello");
+        }
+        else{
+            System.out.println("Bye");
+        }
+
+
+        //comparison btw threee we use else if
+        int p=45,q=67,r=98;
+        if(p> q && p>r)
+            System.out.println(p);
+        else if(q>r && q>r)
+            System.out.println(q);
+        else
+            System.out.println(r);
+
+
+        //check if even odd using TERNARY OPERATOR
+        int m =9;
+        int resultEvenOdd=0;
+        resultEvenOdd = m % 2 == 0 ? 10 : 20;
+        System.out.println(resultEvenOdd);
+
+    }
+}
