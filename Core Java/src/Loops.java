8f7a2a543ac316a43f4eb766f3792ef05556a347
Ojasvi Jaiswal <pankhudij19@gmail.com>
2025-11-13 00:18:55 +0530

    Updated Hello.java with student performance logic

---
+++ Core Java/src/Loops.java
@@ -0,0 +1,50 @@
+public class Loops {
+    public static void main(String[] args) {
+
+        //while loop
+//      //  int i=1;
+//        while(i<=6){
+//            System.out.println(i);
+//            i++;
+//        }
+       // System.out.println("Done");
+
+        //nested loop
+//        int  i=1;
+//        while(i<=4){
+//            System.out.println("Hii : " +i);
+//            int j=1;
+//            while(j<=2){
+//                System.out.println( "I am good : "+j);
+//                j++;
+//            }
+//            i++;
+       // }
+
+        //for loop
+//        for(int i=1;i<=4;i++){
+//            System.out.println(i);
+//        }
+//
+//        for(int i=4;i>=1;i--){
+//            System.out.println(i);
+//        }
+
+//        int i, j;
+//
+//        i = 100;
+//
+//        j = 300;
+//
+//        while(++i < --j);
+//
+//        System.out.println(i);
+
+
+        int x = 5;
+
+        int y = 10;
+
+        int z = (x++ > 5 && y-- < 10) ? x-- : y;
+    }
+}
