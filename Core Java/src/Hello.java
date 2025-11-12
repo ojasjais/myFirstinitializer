fc42b3de962503a80f578abe8a5d9e8f85bb97ea
Ojasvi Jaiswal <pankhudij19@gmail.com>
2025-11-12 18:14:27 +0530

    Updated Hello.java with student performance logic

--- Core Java/src/Hello.java
+++ Core Java/src/Hello.java
@@ -18,8 +18,8 @@ public class Hello {
         System.out.println("String: "+name);
 
         //TypeCasting
-       int n= 10;
-       double d=n;
+       int k= 10;
+       double d=k;
         System.out.println("Widening: "+d);
 
         double d1=10.99;
@@ -66,5 +66,50 @@ public class Hello {
         resultEvenOdd = m % 2 == 0 ? 10 : 20;
         System.out.println(resultEvenOdd);
 
+
+        //Switch case Statement
+        int n=8;
+//        if(n==1)
+//            System.out.println("Monday");
+//        else if(n==2)
+//            System.out.println("Tuesday");
+//        else if(n==3)
+//            System.out.println("Wednesday");
+//        else if(n==4)
+//            System.out.println("Thursday");
+//        else if(n==5)
+//            System.out.println("Friday");
+//        else if(n==6)
+//            System.out.println("Saturday");
+//        else if(n==7)
+//            System.out.println("Sunday");
+
+
+        switch (n){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+                default:
+                    System.out.println("Invalid value");
+
+         }
     }
 }
